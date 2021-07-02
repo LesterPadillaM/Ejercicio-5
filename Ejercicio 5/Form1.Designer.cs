1663bf4b7b225ee81d60faf5c6bdfcce7b77ac88
LesterPadillaM <Lester.padilla@unah.hn>
2021-07-02 17:11:49 -0600

    primer Commit
    Ejercicio terminado, Programacion 3, Lester Padilla, 20191003860

---
+++ Ejercicio 5/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿
+namespace Ejercicio_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvmatriz = new System.Windows.Forms.DataGridView();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnlimpiar = new System.Windows.Forms.Button();
+            this.btncalcular = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtcolumna = new System.Windows.Forms.TextBox();
+            this.txtfila = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvmatriz)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgvmatriz
+            // 
+            this.dgvmatriz.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvmatriz.Location = new System.Drawing.Point(12, 109);
+            this.dgvmatriz.Name = "dgvmatriz";
+            this.dgvmatriz.Size = new System.Drawing.Size(384, 205);
+            this.dgvmatriz.TabIndex = 5;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnlimpiar);
+            this.groupBox2.Controls.Add(this.btncalcular);
+            this.groupBox2.Location = new System.Drawing.Point(219, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(177, 90);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Botones";
+            // 
+            // btnlimpiar
+            // 
+            this.btnlimpiar.Location = new System.Drawing.Point(96, 37);
+            this.btnlimpiar.Name = "btnlimpiar";
+            this.btnlimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnlimpiar.TabIndex = 1;
+            this.btnlimpiar.Text = "Limpiar";
+            this.btnlimpiar.UseVisualStyleBackColor = true;
+            this.btnlimpiar.Click += new System.EventHandler(this.btnlimpiar_Click);
+            // 
+            // btncalcular
+            // 
+            this.btncalcular.Location = new System.Drawing.Point(14, 37);
+            this.btncalcular.Name = "btncalcular";
+            this.btncalcular.Size = new System.Drawing.Size(75, 23);
+            this.btncalcular.TabIndex = 0;
+            this.btncalcular.Text = "Calcular";
+            this.btncalcular.UseVisualStyleBackColor = true;
+            this.btncalcular.Click += new System.EventHandler(this.btncalcular_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtcolumna);
+            this.groupBox1.Controls.Add(this.txtfila);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 90);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Limites de Matriz";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Numero de columnas";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Numero de filas";
+            // 
+            // txtcolumna
+            // 
+            this.txtcolumna.Location = new System.Drawing.Point(119, 51);
+            this.txtcolumna.Name = "txtcolumna";
+            this.txtcolumna.Size = new System.Drawing.Size(75, 20);
+            this.txtcolumna.TabIndex = 4;
+            this.txtcolumna.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtcolumna_KeyPress);
+            // 
+            // txtfila
+            // 
+            this.txtfila.Location = new System.Drawing.Point(119, 22);
+            this.txtfila.Name = "txtfila";
+            this.txtfila.Size = new System.Drawing.Size(75, 20);
+            this.txtfila.TabIndex = 5;
+            this.txtfila.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtfila_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(410, 326);
+            this.Controls.Add(this.dgvmatriz);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Ejercicio 5";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvmatriz)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvmatriz;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnlimpiar;
+        private System.Windows.Forms.Button btncalcular;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtcolumna;
+        private System.Windows.Forms.TextBox txtfila;
+    }
+}
+
---
+++ Ejercicio 5/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio_5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btncalcular_Click(object sender, EventArgs e)
+        {
+            if (txtfila.Text == "" || txtcolumna.Text == "")
+            {
+                MessageBox.Show("Datos incompletos");
+            }
+            else
+            {
+                int n = Convert.ToInt32(txtfila.Text);
+                int m = Convert.ToInt32(txtcolumna.Text);
+                int[,] matriz = new int[n, m];
+                Random r = new Random();
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < m; j++)
+                    {
+                        matriz[i, j] = r.Next(1, 51);
+                    }
+                }
+                dgvmatriz.ColumnCount = m;
+                dgvmatriz.RowCount = n;
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < m; j++)
+                    {
+                        dgvmatriz.Rows[i].Cells[j].Value = matriz[i, j].ToString();
+                    }
+                }
+                txtcolumna.Enabled = false;
+                txtfila.Enabled = false;
+            }
+        }      
+
+        private void btnlimpiar_Click(object sender, EventArgs e)
+        {
+            txtfila.Enabled = true;
+            txtcolumna.Enabled = true;
+            txtfila.Text = "";
+            txtcolumna.Text = "";
+            dgvmatriz.Columns.Clear();
+        }
+
+        private void txtfila_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(char.IsNumber(e.KeyChar)) && (e.KeyChar != (char)Keys.Back))
+            {
+                MessageBox.Show("Solo se permiten numeros", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                e.Handled = true;
+                return;
+            }
+        }
+
+        private void txtcolumna_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(char.IsNumber(e.KeyChar)) && (e.KeyChar != (char)Keys.Back))
+            {
+                MessageBox.Show("Solo se permiten numeros", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                e.Handled = true;
+                return;
+            }
+        }
+    }
+}
